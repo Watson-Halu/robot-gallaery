bd0c54ad41a9e9122975f7cc7084d432fb35cc91
watson <watson@okmaster.com>
2021-07-02 01:36:52 +0800

    feature/ import shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 // import "./index.css";
 import styles from "./App.module.css";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <img src={logo} className={styles.appLogo} alt="logo" />
         <h1>機器人的列表來試試看這段文字可以有多長</h1>
       </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robots.map((r) => {
           return <Robot id={r.id} name={r.name} email={r.email} />;
