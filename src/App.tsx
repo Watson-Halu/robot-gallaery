828d14e2491d2e1dc4f53b94bd40248a72127558
watson <watson@okmaster.com>
2021-07-01 17:16:43 +0800

    feature/ update logo import url & add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/image/logo.svg";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 // import "./index.css";
@@ -8,6 +8,10 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>機器人的列表來試試看這段文字可以有多長</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map((r) => {
           return <Robot id={r.id} name={r.name} email={r.email} />;
